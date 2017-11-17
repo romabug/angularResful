6ae23ee238fb0feb1fffc39b159ff6941aee08b8
Hanchang cai <hanchangcai@gmail.com>
2017-11-17 15:51:43 +1100

    change

--- 学习笔记.js
+++ 学习笔记.js
@@ -1,33 +1,23 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+CLI常用命令
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
-};
+ng new project-name - 创建一个新项目，置为默认设置
+ng new project_name --routing   (pls routing files)
+
+
+ng build - 构建/编译应用
+ng test - 运行单元测试
+ng e2e - 运行端到端（end-to-end）测试
+ng serve - 启动一个小型web服务器，用于托管应用
+ng deploy - 即开即用，部署到Github Pages或者Firebase
+
+
+     组件| ng g component home/component/my-new-component     
+    //@@ 相对生成组件生成位置在项目的根目录的  
+    //src/app/home/component（指令其他等等都可以用该方式生成）
+    指令| ng g directive my-new-directive 
+    管道| ng g pipe my-new-pipe 
+    服务| ng g service my-new-service 
+      类| ng g class my-new-class 
+    接口| ng g interface my-new-interface 
+    枚举| ng g enum my-new-enum 
+    模块|ng g module my-module
